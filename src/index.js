aa1a29059dd7619c3bc4a8c3532d4874666ee877
Raphael Gomide <raphael.gomide@gmail.com>
2021-01-05 12:54:09 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/styles.css';
+
+function start() {
+  const root = document.getElementById('root');
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Boilerplate';
+  root.appendChild(h1);
+}
+
+start();
